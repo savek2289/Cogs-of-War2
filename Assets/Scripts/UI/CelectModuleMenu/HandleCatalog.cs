b2a12e2310087dc311e488ae071adde45eaf8c96
user <user@mail.com>
2026-03-09 11:23:06 +0300

    Доделаны разделы для робота

--- Assets/Scripts/UI/CelectModuleMenu/HandleCatalog.cs
+++ Assets/Scripts/UI/CelectModuleMenu/HandleCatalog.cs
@@ -48,14 +48,61 @@ public class HandleCatalog : MonoBehaviour
         {
             items[i].TryGetComponent(out Button button);
             items[i].TryGetComponent(out ScrollViewHoverHandler handler);
+            items[i].TryGetComponent(out Image image);
+            GameObject child1 = items[i].transform.GetChild(0).gameObject;
+            GameObject child2 = items[i].transform.GetChild(1).gameObject;
+            GameObject modulesContainer = items[i].transform.GetChild(2).gameObject;
+
+            if (child1 != null)
+                child1.gameObject.SetActive(false);
+            
+            if (child2 != null)
+                child2.gameObject.SetActive(false);
 
             button.enabled = false;
             handler.enabled = false;
+            image.enabled = false;
+
+            if (modulesContainer == null) return;
+
+            for (int j = 1; j < modulesContainer.transform.childCount; j++)
+            {
+                GameObject module = modulesContainer.transform.GetChild(j).gameObject;
+                module.SetActive(true);
+            }
         } 
     }
 
     public void ResetCatalog()
     {
         title.text = "Modules";
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            items[i].TryGetComponent(out Button button);
+            items[i].TryGetComponent(out ScrollViewHoverHandler handler);
+            items[i].TryGetComponent(out Image image);
+            GameObject child1 = items[i].transform.GetChild(0).gameObject;
+            GameObject child2 = items[i].transform.GetChild(1).gameObject;
+            GameObject modulesContainer = items[i].transform.GetChild(2).gameObject;
+
+            if (child1 != null)
+                child1.gameObject.SetActive(true);
+
+            if (child2 != null)
+                child2.gameObject.SetActive(true);
+
+            button.enabled = true;
+            handler.enabled = true;
+            image.enabled = true;
+
+            if (modulesContainer == null) return;
+
+            for (int j = 0; j < modulesContainer.transform.childCount; j++)
+            {
+                GameObject module = modulesContainer.transform.GetChild(j).gameObject;
+                module.SetActive(false);
+            }
+        }
     }
 }
